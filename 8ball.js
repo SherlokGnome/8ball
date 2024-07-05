679a28751b52527158ea5d2293a57ec879d096ab
Your Name <your.email@example.com>
2024-07-05 16:59:54 +1200

    modified:   8ball.js

--- 8ball.js
+++ 8ball.js
@@ -1,38 +1,55 @@
 //Variables
-let userName = "";
-let userQuestion = "";
+let userName = "sherlock";
+let userQuestion = "who are you?";
 let randomNumber = Math.floor(Math.random() * 8);
 let eightball = "";
 
 userName === "" ? console.log(` Hello ${userName}!`) : console.log("Hello!");
 console.log(`${userName} is asking ${userQuestion}`);
+/*
+if (randomNumber == 1) {
+  eightball = "It is certain";
+} else if (randomNumber == 2) {
+  eightball = "It is decidedly so";
+} else if (randomNumber == 3) {
+  eightball = "Reply hazy try again";
+} else if (randomNumber == 4) {
+  eightball = "Cannot predict now";
+} else if (randomNumber == 5) {
+  eightball = "Do not count on it";
+} else if (randomNumber == 6) {
+  eightball = "My sources say no";
+} else if (randomNumber == 7) {
+  eightball = "Outlook not so good";
+} else {
+  eightball = "Signs point to yes";
+}
 
-switch (eightball) {
-  case "1":
-    console.log('It is certain');
+console.log(eightball);*/
+console.log(randomNumber)
+
+switch (randomNumber ) {
+  case 1:
+    console.log("It is certain");
     break;
-  case "2":
-    console.log('It is decidedly so');
+  case 2:
+    console.log("It is decidedly so");
     break;
-  case "3":
-    console.log('Reply hazy try again');
+  case 3:
+    console.log("Reply hazy try again");
     break;
-  case "4":
-    console.log('Cannot predict now');
+  case 4:
+    console.log("Cannot predict now");
     break;
-  case "5":
-    console.log('Do not count on it');
+  case 5:
+    console.log("Do not count on it");
     break;
-  case "6":
-    console.log('My sources say no');
+  case 6:
+    console.log("My sources say no");
     break;
-  case "7":
-    console.log('Outlook not so good');
-    break;
-  case "8":
-    console.log('Signs point to yes');
+  case 7:
+    console.log("Outlook not so good");
     break;
     default:
-        console.log('Please ask a question')
-
+    console.log("Signs point to yes");
 }
